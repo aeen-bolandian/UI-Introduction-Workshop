1ea60dd27adb0c3c6bdb9238dd7647a609860ab4
aeen bolandian <bolandian85@gmail.com>
2025-04-24 00:15:38 +0330

    dark/light mode key has been added

--- src/main/java/org/example/javafx/PricingController.java
+++ src/main/java/org/example/javafx/PricingController.java
@@ -2,12 +2,18 @@ package org.example.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PricingController {
 
@@ -85,5 +91,23 @@ public class PricingController {
             label = label3;
             label.setFont(originalFont3);
         }
+    }
+
+    @FXML
+    public void switchToNightMode(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("pricing-view-dark.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchToLightMode(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("pricing-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 }
